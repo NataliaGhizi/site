5eb0ac5fe4d3ee485a45864b5d40c9c2b172dd4e
Natalia Ghizi <natalia.gdesouza@gmail.com>
2019-03-24 23:25:46 -0300

    le arqs

--- testes.py
+++ testes.py
@@ -1,8 +1,18 @@
 import csv
+import random
+nomes = []
+sobrenomes = []
+with open('arq/nomes.csv', newline='') as f:
+    arq = csv.reader(f, delimiter=';', quoting=csv.QUOTE_NONE)
+    count = 0
+    for row in arq:
+        nomes.append(row[count])
+        count = + count
+with open('arq/sobrenomes.csv', newline='') as f:
+    arq = csv.reader(f, delimiter=';', quoting=csv.QUOTE_NONE)
+    count = 0
+    for row in arq:
+        sobrenomes.append(row[count])
+        count = + count
 
-arquivo = open('arq/nomes.csv')
-
-linhas = csv.reader(arquivo)
-
-for linha in linhas:
-    print(linha,'\n')
+print(nomes[random.randrange( len (nomes))] , sobrenomes[random.randrange (len (sobrenomes))] )
